19e5a2c26cfcfb06669aa1cdc6f23e0c994bf09e
Koriet520 <jorge-52099@hotmail.com>
2018-12-10 17:07:23 -0600

    Hola

---
+++ js/prubea.js
@@ -0,0 +1,9 @@
+    $(document).ready(function(){
+      $('.your-class').slick({
+        slidesToShow: 3,
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+      });
+    });
