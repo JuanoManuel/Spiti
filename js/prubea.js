53bad5d5d0ea40d5e28b5d7601978d3dce3f6fda
JuanoManuel <juan_mhh@outlook.com>
2018-12-19 15:34:31 -0600

    Ahora si ya mero termino

--- js/prubea.js
+++ js/prubea.js
@@ -1,9 +1,21 @@
     $(document).ready(function(){
       $('.your-class').slick({
+      	centerMode: false,
         slidesToShow: 3,
         accessibility: true,
         autoplay: true,
         autoplaySpeed: 3000,
         arrows: false,
+        responsive: [
+	        {
+	        	breakpoint: 540,
+	        	settings: {
+	        		arrows: false,
+	        		centerMode: true,
+	        		centerPadding: '60px',
+	        		slidesToShow: 1
+	        	}
+	        }
+        ]
       });
     });
